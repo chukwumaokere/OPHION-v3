5fe596fc75981e8c246eb696cb85be3f2f264124
Chukwuma Okere <chukwumaokere@Yahoo.com>
2016-01-19 06:13:32 -0600

    added proper logout screen with splash, added redirect logout
    need to configure new user form, and db work on it, and also summary of
    product

--- login.php
+++ login.php
@@ -2,7 +2,8 @@
 $user = $_POST['user'];
 $password= $_POST['password'];
 
-session_start();
+session_start(['cookie_lifetime' => 600]);
 $_SESSION['user'] = $user;
+$_SESSION['password'] = $password;
 
 header("location:./welcome.html");
