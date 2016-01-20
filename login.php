990202d425a4ea4da75e713171fc67fa45bf70a1
Chukwuma Okere <chukwumaokere@Yahoo.com>
2016-01-20 16:19:31 -0600

    added split welcome screens and login account type check

--- login.php
+++ login.php
@@ -1,9 +1,20 @@
 <?php
 $user = $_POST['user'];
 $password= $_POST['password'];
+$user_type = $_POST['user_type'];
+
 
 session_start(['cookie_lifetime' => 600]);
 $_SESSION['user'] = $user;
 $_SESSION['password'] = $password;
 
-header("location:./welcome.html");
+if ($user_type == 0) {
+	header("location:./welcomeemployer.html");
+}
+else if ($user_type == 1) {
+	header("location:./welcome.html");
+}
+	
+else {
+	header("location:./login.html");
+}
