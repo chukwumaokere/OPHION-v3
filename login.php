5c7a12a4d7b0cff22962638f1c8b0c6671e7b22a
Chukwuma Okere <chukwumaokere@Yahoo.com>
2016-01-25 02:55:28 -0600

    added login username prompt, added login validation, added password hashing to db, added new user empty prompts, added terms and agreements

--- login.php
+++ login.php
@@ -1,15 +1,67 @@
 <?php
+//begin session
 session_start(['cookie_lifetime' => 600]);
 
-$user = $_POST['user'];
-$password= $_POST['password'];
+//variables needed
+$username = $_POST['username'];
+$password = $_POST['password'];
+$hashpass = password_hash($_POST['password'], PASSWORD_DEFAULT);
 $user_type = $_POST['user_type'];
 
-$_SESSION['user'] = $user;
+//session storage
+$_SESSION['username'] = $username;
 $_SESSION['password'] = $password;
 $_SESSION['user_type'] = $user_type;
+$_SESSION[$hashpass] = $hashpass;
 
-if ($user_type == 0) {
+//db values
+$servername = '66.228.53.178';
+$dbusername   = 'chukwuma';
+$dbpassword   = 'Z_Q7tM"VQaGyqx3n';
+$dbname     = 'chukwuma_ophion';
+
+//db connect
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword); //connects to database
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //shows errors
+
+//login validation
+function getHashedPassword($username, $user_type, $conn) {
+	$stmt = $conn->prepare('SELECT password FROM user WHERE username = :username AND user_type = :user_type');
+	$stmt->execute(
+			array(
+					'username'=>$username,
+					'user_type'=>$user_type
+			)
+			);
+	$row = $stmt->fetch();
+	
+	if ($row == false) {
+		return false;
+	}
+	return $row['password'];
+	//var_dump($row);
+}
+$passwordHash = getHashedPassword($username, $user_type, $conn);
+password_verify($password, $passwordHash);
+
+//var_dump(getHashedPassword($username, $user_type, $conn));
+
+if (password_verify($password, $passwordHash) == true && $user_type == 0) {
+	header("location:./welcomeemployer.html");
+}
+	else if (password_verify($password, $passwordHash) == true && $user_type == 1) {
+		header("location:./welcome.html");
+	}
+	
+	else if (password_verify($password, $passwordHash) == false) {
+		echo "Invalid Login Credentials. Please try again";
+		header("refresh: 3;url=./login.html");
+		//header("location:./incorrectlogin.html") //when released
+	}
+
+
+/* //check user type
+ if ($user_type == 0) {
 	header("location:./welcomeemployer.html");
 }
 else if ($user_type == 1) {
@@ -18,7 +70,7 @@ else if ($user_type == 1) {
 	
 else {
 	header("location:./login.html");
-}
-
+} */
+ 
 //this should actually check login credentials with db
 //if user_type in user in chukwuma_ophion = 0 or 1
